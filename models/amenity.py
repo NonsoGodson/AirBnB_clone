a18f200b94b42b095cd04b997411196f870ac81d
warengeorge <georgeomiebi@gmail.com>
2022-11-28 22:08:25 +0100

    correct models

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Class Amenity """
+"""Contains the Amenity model"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Amenity class that inherits BaseModel"""
+    """Implements the Amenity model"""
     name = ""
